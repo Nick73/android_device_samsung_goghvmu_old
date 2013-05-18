1737e6a222bdb8a8dba2835ba82b5cae1941c80f
Joey Troy <joey@joeytroy.com>
2013-05-18 17:05:40 -0600

    first commit

---
+++ cm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/samsung/gogvmu/full_gogvmu.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=goghvmu TARGET_DEVICE=goghvmu BUILD_FINGERPRINT="samsung/goghvmu/goghvmu:4.1.2/JZO54K/L300VPAMC1:user/release-keys"
+
+PRODUCT_NAME := cm_goghvmu
+PRODUCT_DEVICE := goghvmu
+
